c5da1802b711644ab3bb3e95e7a526964a3d704c
ARitz Cracker <aritz@aritzcracker.ca>
2024-12-02 22:02:15 -0500

    i just want this done aaaaaaaaa

--- drakermore-server/src/main.rs
+++ drakermore-server/src/main.rs
@@ -257,7 +257,7 @@ async fn get_pw_mod_metadata(AxumPath(mut jar_file_name_str): AxumPath<String>) 
 async fn get_pw_copy_metadata(AxumPath(mut copy_file_name_str): AxumPath<String>) -> Response {
 	ok_or_anyhow_response(
 		async {
-			if !copy_file_name_str.ends_with(".pw.toml") {
+			if !copy_file_name_str.ends_with(".pw.toml") || !copy_file_name_str.starts_with("/packwiz/") {
 				return Err(IoError::new(
 					IoErrorKind::NotFound,
 					format!("cannot find {copy_file_name_str} in this folder"),
@@ -265,7 +265,7 @@ async fn get_pw_copy_metadata(AxumPath(mut copy_file_name_str): AxumPath<String>
 				.into());
 			}
 			copy_file_name_str.truncate(copy_file_name_str.len() - "pw.toml".len());
-			let copy_file_name = PathBuf::from(copy_file_name_str.clone());
+			let copy_file_name = PathBuf::from(copy_file_name_str.split_off("/packwiz/".len()));
 			let mut full_path = CLI_OPTIONS.config.clone();
 			full_path.push(copy_file_name);
 			Ok(pw_copy_metadata_string(&full_path).await?)
